2fbac61a198659f8b1c0f899c90d7a73eff70a21
anvaygrover <anvay11@gmail.com>
2022-04-12 17:13:44 -0500

    Simple inline tests

--- lib/catamorphisms.ml
+++ lib/catamorphisms.ml
@@ -11,6 +11,7 @@
 type nat = 
   | Zero 
   | Succ of nat
+[@@deriving show]
 
 let rec cataNat (n : nat) (zero_case : 'a) (succ_case : 'a -> 'a) : 'a =
   match n with
@@ -18,5 +19,20 @@ let rec cataNat (n : nat) (zero_case : 'a) (succ_case : 'a -> 'a) : 'a =
   | Succ(n') -> succ_case (cataNat n' zero_case succ_case)
 
 (* Instantiate cataNat for simple successor function *)
-let successor n = cataNat n Zero (fun n -> Succ n)
+let cataSucc n = cataNat n (Succ Zero) (fun n -> Succ n)
 
+(* inline tests on successor *)
+let%test _ = cataSucc (Succ Zero) = Succ (Succ Zero)
+
+
+let rec cataList (l : 'a list) (nil_case : 'b) (cons_case : 'a -> 'b -> 'b) : 'b =
+  match l with
+  | [] -> nil_case
+  | hd :: tl ->  cons_case hd (cataList tl nil_case cons_case)
+
+
+(* Instantiate cataList for length function *)
+let cataLength l = cataList l 0 (fun _ b -> 1 + b)
+
+(* TODO: write tests on length function *)
+let%test _ = cataLength (1 :: (2 ::[])) = 2
